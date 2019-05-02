2d2a3d31851d5431f9a47e2748a7d9072ff1ce99
ItsLake <vladislav.shishatskiy@gmail.com>
2019-05-02 11:53:57 +0300

    Merge branch 'Update_27.04' of github.com:/ItsLake/Cisman2111 into RFVFV
    # Conflicts:
    #	src/Contact.java
    deferve
    UpDate)))V2

--- src/DateBaseHandler.java
+++ src/DateBaseHandler.java
@@ -17,48 +17,16 @@ public class DateBaseHandler extends Config {
         dbConnection = DriverManager.getConnection(connectionString, dbUser,dbPass);
         return dbConnection;
     }
-
-    /*public void singUpUser(String Name, String UserName, String Partronymic, String Pulpit,
-                           String Profession, String Contactt, String Training){
-        String insert = "INSERT INTO " + Const.USER_TABLE + "(" + Const.NAME + "," + Const.USERNAME + "," + Const.PARTRONYMIC +
-                "," + Const.PULPIT + "," + Const.PROFESSION + "," + Const.CONTACTT + "," + Const.TRAINING +")" + "VALUES(?,?,?,?,?,?,?)";
-
-        int coun = 0;
-        try {
-            PreparedStatement preparedStatement = getDbConnection().prepareStatement(insert);
-            preparedStatement.setString(2, UserName);
-            preparedStatement.setString(3, Partronymic);
-            preparedStatement.setString(4, Pulpit);
-            preparedStatement.setString(5,Profession);
-            preparedStatement.setString(6, Contactt);
-            preparedStatement.setString(7, Training);
-            preparedStatement.setString(1, Name);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        if(coun >=1){
-            ContactV2.SaveMes.setVisible(true);
-        }else {
-
-        }
-    }*/
-
     public void singUpUser(String name []){
         String insert = "INSERT INTO " + Const.USER_TABLE + "(" + Const.NAME + "," + Const.USERNAME + "," + Const.PARTRONYMIC +
                 "," + Const.PULPIT + "," + Const.PROFESSION + "," + Const.CONTACTT + "," + Const.PREPOD + "," +
                 Const.TRAINING + "," + Const.STUDANT + "," + Const.TRAININGMAIN + "," + Const.PARA +  ")" + "VALUES(?,?,?,?,?,?,?,?,?,?,?)";
-
         int coun = 0;
         try {
             PreparedStatement preparedStatement = getDbConnection().prepareStatement(insert);
             for (int s = 0; s < name.length; s++){
                 preparedStatement.setString(s+1,name[s]);
             }
-
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -71,9 +39,6 @@ public class DateBaseHandler extends Config {
 
         }
     }
-
-
-
     public ResultSet getUser( String UserName){
         ResultSet resultSet = null;
 
@@ -119,7 +84,6 @@ public class DateBaseHandler extends Config {
         }
         return null;
     }
-
     public ResultSet seart(String UserName) {
         ResultSet resultSet = null;
 
