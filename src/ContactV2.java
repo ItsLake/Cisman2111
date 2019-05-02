802e47389475db49c950958188784e7dc1750bd6
ItsLake <vladislav.shishatskiy@gmail.com>
2019-05-02 04:12:19 +0300

    Merge branch 'Update_27.04' of github.com:/ItsLake/Cisman2111 into RFVFV
    # Conflicts:
    #	src/Contact.java
    deferve
    UpDate)))

--- src/ContactV2.java
+++ src/ContactV2.java
@@ -1,7 +1,4 @@
 import javax.swing.*;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
-import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.ResultSet;
@@ -28,25 +25,24 @@ public class ContactV2 extends JFrame {
     private JLabel TextPulpit = new JLabel("Кафедра");
     private JLabel TextProfession = new JLabel("Спеціальність");
     private JLabel TextContact  = new JLabel("Номер");
-    private JLabel TextIntarn;
+    private JLabel TextIntarn = new JLabel("Курс/Форма");
     private JLabel TextPrepod = new JLabel("Викладач");
     private JLabel NameTraining = new JLabel("Тренінг");
     private JLabel ParaText = new JLabel("Пара");
     static JLabel SaveMes = new JLabel("SAVE");
-    static JLabel ErrorMes;
+    static JLabel ErrorMes = new JLabel("NO DATE");
 
-
-    private JComboBox Training;
-    private JComboBox Studant;
-    private JComboBox TrainingMain;
-    private JComboBox Para;
-    private JComboBox PeopleList = new JComboBox();
+    private JComboBox Training= new JComboBox();
+    private JComboBox Studant = new JComboBox();
+    private JComboBox TrainingMain= new JComboBox();
+    private JComboBox Para= new JComboBox();
+    private JComboBox PeopleList= new JComboBox();
 
     private JButton Back = new JButton("Назад");
     private JButton Save = new JButton("Зберегти");
 
-    private JList<String> TEst;
-    private JScrollPane jScrollPane;
+    private JList<String> ListName = new JList<>();
+    private JScrollPane jScrollPane = new JScrollPane();
 
 
     private DefaultListModel defaultListModel = new DefaultListModel();
@@ -54,6 +50,7 @@ public class ContactV2 extends JFrame {
     private static String namee ;
     private static String getItem [] = new String[24];
     int coun ;
+    int test ;
 
 
     public ContactV2() {
@@ -61,66 +58,17 @@ public class ContactV2 extends JFrame {
         this.setBounds(50, 50, 620, 420);
         getContentPane().add(vkladki);
 
-        One.setLayout(null);
-        Two.setLayout(null);
+        JP(One,"Заняття");
+        JP(Two,"Тренінг");
 
-        vkladki.add("Заняття", One);
-        vkladki.add("Тренінг", Two);
+        Search(search,false,125,92,150,120);
+        JSro(jScrollPane,ListName);
 
-        search.setLayout(new BorderLayout());
-        search.setBounds(125,93,150,70);
-        search.setVisible(false);
-        One.add(search);
-
-        TEst = new JList<>(defaultListModel);
-        TEst.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        TEst.setLayoutOrientation(JList.VERTICAL);
-        TEst.setVisibleRowCount(5);
-        TEst.setBounds(0,0,150,20);
-
-        jScrollPane = new JScrollPane();
-        jScrollPane.setViewportView(TEst);
-        search.add(jScrollPane);
-
-        One.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                search.setVisible(false);
-            }
-        });
-
-       TEst.addMouseListener(new MouseAdapter() {
-           @Override
-           public void mouseClicked(java.awt.event.MouseEvent e) {
-               super.mouseClicked(e);
-               SurName.setText(TEst.getSelectedValue());
-               ResultSet res = dateBaseHandler.seart(SurName.getText());
-               coun = 0;
-               try {
-                   while (res.next()) {
-                       coun++;
-                        Contactt.setText(res.getString(Const.CONTACTT));
-                        Partronymic.setText(res.getString(Const.PARTRONYMIC));
-                        Profession.setText(res.getString(Const.PROFESSION));
-                        Pulpit.setText(res.getString(Const.PULPIT));
-                        Name.setText(res.getString(Const.NAME));
-                   }
-               }catch (SQLException a){
-                   a.printStackTrace();
-               }
-               search.setVisible(false);
-           }
-       });
-
+        String[] ite = {"1", "2", "3"};
+        String[] item = {"Бюджет", "Контрак"};
+        String[] NamberPara = {"1", "2", "3", "4"};
         String NamePeople [] = {"Інтерн","Слухач", "Відвідувач","Лікарь","Парамед","Сер.Мед. персонал"};
-        People(NamePeople);
-
         String TrainingMainText[] = {"Серцево-легенева реанімація", "Люмбальна пункція","В/в, в/м ін’єкції", "Постановка переферичного венозного катетеру", "Катетеризація центральної вени", "Крикотиреотомія", "Плевральна пункція", "Торакоцентез"};
-
-        TrainingMain = new JComboBox(TrainingMainText);
-        TrainingMain.setBounds(260, 20, 325, 25);
-        One.add(TrainingMain);
 
         JText(Name,125,105,150,25);
         JText(Pulpit,435, 70, 150, 25);
@@ -131,14 +79,29 @@ public class ContactV2 extends JFrame {
         JText(Partronymic,125, 140, 150, 25);
 
         BoundsFont(TextName,16,15,105,70,20);
+        BoundsFont(SaveMes,16,460,300,250,25);
         BoundsFont(TextPulpit,16,300,70,80,20);
         BoundsFont(TextSurName,16,15,70,100,20);
         BoundsFont(ParaText,16,15, 240, 90, 20);
         BoundsFont(TextPrepod,16,300,140,140,25);
         BoundsFont(NameTraining,19,150,20,110,20);
+        BoundsFont(ErrorMes,16,250, 320, 250, 25);
         BoundsFont(TextContact,16,15, 175, 90, 20);
         BoundsFont(TextPatronymic,16,15,140,140,20);
+        BoundsFont(TextIntarn,16,300, 175, 120, 20);
         BoundsFont(TextProfession,16,300,105,140,20);
+
+        JBut(Save,16,300, 300, 150, 30);
+        JBut(Back,16,150, 300, 150, 30);
+
+        JCom(Studant,ite,435, 175, 50, 25);
+        JCom(Training,item,485, 175, 100, 25);
+        JCom(Para,NamberPara,125, 240, 90, 25);
+        JCom(PeopleList,NamePeople,15, 20, 110, 25);
+        JCom(TrainingMain,TrainingMainText,260, 20, 325, 25);
+
+        Mess(SaveMes,Color.GREEN,false);
+        Mess(ErrorMes,Color.RED,false);
 
         SurName.addKeyListener(new KeyAdapter() {
             @Override
@@ -156,7 +119,6 @@ public class ContactV2 extends JFrame {
                         namee = res.getString(Const.USERNAME);
                         defaultListModel.addElement(namee);
                         getItem [coun] = namee;
-
                     }
                 }catch (SQLException a){
                     a.printStackTrace();
@@ -165,50 +127,40 @@ public class ContactV2 extends JFrame {
                     search.setVisible(false);
                     defaultListModel.removeAllElements();
                 }else search.setVisible(true);
+                JList(ListName ,defaultListModel,5,0,0,150,20);
+                search.setBounds(125,92,150,AutoScl());
+            }
+        });
+        One.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                search.setVisible(false);
             }
         });
 
-        String[] NamberPara = {"1", "2", "3", "4"};
-
-        Para = new JComboBox(NamberPara);
-        Para.setBounds(125, 240, 90, 25);
-        One.add(Para);
-
-        String[] item = {"Бюджет", "Контрак"};
-
-        Training = new JComboBox(item);
-        Training.setBounds(485, 175, 100, 25);
-        One.add(Training);
-
-        TextIntarn = new JLabel("Курс/Форма");
-        TextIntarn.setFont(new Font("serif", Font.PLAIN, 16));
-        TextIntarn.setBounds(300, 175, 120, 20);
-        One.add(TextIntarn);
-
-        String[] ite = {"1", "2", "3"};
-        Studant = new JComboBox(ite);
-        Studant.setBounds(435, 175, 50, 25);
-        One.add(Studant);
-
-        Save.setFont(new Font("serif", Font.PLAIN, 16));
-        Save.setBounds(300, 300, 150, 30);
-        One.add(Save);
-
-        Back.setFont(new Font("serif", Font.PLAIN, 16));
-        Back.setBounds(150, 300, 150, 30);
-        One.add(Back);
-
-        ErrorMes = new JLabel("NO DATE");
-        ErrorMes.setForeground(Color.RED);
-        ErrorMes.setBounds(250, 320, 250, 25);
-        ErrorMes.setFont(new Font("serif", Font.PLAIN, 16));
-        One.add(ErrorMes);
-        ErrorMes.setVisible(false);
-
-        SaveMes.setForeground(Color.GREEN);
-        BoundsFont(SaveMes,16,460,300,250,25);
-        SaveMes.setVisible(false);
-        One.add(SaveMes);
+        ListName.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                super.mouseClicked(e);
+                SurName.setText(ListName.getSelectedValue());
+                ResultSet res = dateBaseHandler.seart(SurName.getText());
+                coun = 0;
+                try {
+                    while (res.next()) {
+                        coun++;
+                        Contactt.setText(res.getString(Const.CONTACTT));
+                        Partronymic.setText(res.getString(Const.PARTRONYMIC));
+                        Profession.setText(res.getString(Const.PROFESSION));
+                        Pulpit.setText(res.getString(Const.PULPIT));
+                        Name.setText(res.getString(Const.NAME));
+                    }
+                }catch (SQLException a){
+                    a.printStackTrace();
+                }
+                search.setVisible(false);
+            }
+        });
 
         Back.addActionListener(new ActionListener() {
             @Override
@@ -224,13 +176,6 @@ public class ContactV2 extends JFrame {
             }
         });
     }
-    public void People(String [] namePeople){
-        for (int e = 0; e < namePeople.length; e++){
-            PeopleList.addItem(namePeople[e]);
-        }
-        PeopleList.setBounds(15, 20, 110, 25);
-        One.add(PeopleList);
-    }
     public void BoundsFont(JLabel name,int font, int x ,int y, int w, int h){
         name.setBounds(x,y,w,h);
         name.setFont(new Font("serif", Font.PLAIN,font));
@@ -240,4 +185,48 @@ public class ContactV2 extends JFrame {
         name.setBounds(x,y,w,h);
         One.add(name);
     }
-}
+    public void JBut(JButton name, int font,int x ,int y, int w, int h){
+        name.setBounds(x,y,w,h);
+        name.setFont(new Font("serif", Font.PLAIN,font));
+        One.add(name);
+    }
+    public void JCom(JComboBox name,String [] tab ,int x ,int y, int w, int h){
+        for(int bly = 0; bly < tab.length; bly++){
+            name.addItem(tab[bly]);
+        }
+        name.setBounds(x,y,w,h);
+        One.add(name);
+    }
+    public void JP(JPanel name , String title){
+            name.setLayout(null);
+            vkladki.add(title ,name);
+        }
+    public void Search(JPanel name, boolean visibility,int x, int y,int w,int h){
+        name.setLayout(new BorderLayout());
+        name.setBounds(x,y,w,h);
+        name.setVisible(visibility);
+        One.add(name);
+    }
+    public void JList(JList name, DefaultListModel title, int RowCount, int x, int y, int w, int h){
+        name.setListData(title.toArray());
+        name.setBounds(x,y,w,h);
+        name.setVisibleRowCount(RowCount);
+        name.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        name.setLayoutOrientation(JList.VERTICAL);
+    }
+    public void Mess(JLabel name, Color color, boolean visible){
+        name.setForeground(color);
+        name.setVisible(visible);
+    }
+    public void JSro(JScrollPane name, JList name1){
+        name.setViewportView(name1);
+        search.add(jScrollPane);
+    }
+    public int AutoScl(){
+        int height;
+        if(defaultListModel.getSize() * 25 > 75){
+            height = 75;
+        }else height = defaultListModel.getSize() * 25;
+        return height;
+    }
+    }
